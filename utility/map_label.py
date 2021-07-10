23c49b1af14d55ada33b64ea34a0c5d46e6f90d8
CaoYmg <1040862379@qq.com>
2021-07-10 17:41:20 +0800

    Create map_label.py
    update ml_100k_label

---
+++ utility/map_label.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+
+
+ml_100k_label = {1: 0, 2: 0, 3: 0, 4: 1, 5: 1}
+
+dataset_label = {
+    'ml_100k': ml_100k_label
+
+}
+
+def map_column(df, col, dictionary):
+    df[col] = df[col].map(dictionary).astype(np.int32)
+    return df
